cde59baac93572470332694175cd401642945dbd
Arrisio <SignumMea@gh1>
2020-05-31 12:46:49 +0300

    feat(homework completed):

---
+++ index.js
@@ -0,0 +1,28 @@
+const http = require('http');
+chance = require('chance').Chance();
+
+const port = process.env.PORT || '3000';
+const CONSOLE_MSG_INTERVAL = process.env.CONSOLE_MSG_INTERVAL || 1000;
+const RESPONSE_DELAY = process.env.RESPONSE_DELAY || 10000;
+
+const server = http.createServer((req, res) => {
+    if (req.method === 'GET') {
+        const client = chance.name()
+        console.log(`Client: ${client} connected!`);
+
+        const intervalID = setInterval(() => {
+            console.log(`Client: ${client}, time: ${new Date().toUTCString()}`);
+        }, CONSOLE_MSG_INTERVAL);
+
+        setTimeout(() => {
+            clearInterval(intervalID);
+            const time = new Date().toUTCString();
+            console.log(`Client: ${client}, time: ${time} end connection`);
+            res.end(`Current time: ${time}`);
+        }, RESPONSE_DELAY);
+    }
+});
+
+server.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
