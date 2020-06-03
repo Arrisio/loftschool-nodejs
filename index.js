182d33ff6cba20d608c4f501f8c9dd7cb47c03db
Arrisio <SignumMea@gh1>
2020-06-03 18:31:17 +0300

    fixed

--- index.js
+++ index.js
@@ -3,19 +3,35 @@ chance = require('chance').Chance();
 
 const port = process.env.PORT || '3000';
 const CONSOLE_MSG_INTERVAL = process.env.CONSOLE_MSG_INTERVAL || 1000;
-const RESPONSE_DELAY = process.env.RESPONSE_DELAY || 10000;
+const RESPONSE_DELAY = process.env.RESPONSE_DELAY || 5000;
+
+let connected_clients = [];
+let intervalConsoleLogger;
+
+const removeClient = client => {
+    connected_clients.splice(connected_clients.indexOf(client), 1);
+    if (connected_clients.length === 0) {
+        clearInterval(intervalConsoleLogger)
+    }
+}
+
+const addClient = client => {
+    if (connected_clients.length === 0) {
+        intervalConsoleLogger = setInterval(() => {
+            console.log(`Client: ${client}, time: ${new Date().toUTCString()}`);
+        }, CONSOLE_MSG_INTERVAL);
+    }
+    connected_clients.push(client);
+}
 
 const server = http.createServer((req, res) => {
     if (req.method === 'GET') {
-        const client = chance.name()
+        const client = chance.name();
+        addClient(client);
         console.log(`Client: ${client} connected!`);
 
-        const echoInterval = setInterval(() => {
-            console.log(`Client: ${client}, time: ${new Date().toUTCString()}`);
-        }, CONSOLE_MSG_INTERVAL);
-
         setTimeout(() => {
-            clearInterval(echoInterval);
+            removeClient(client);
             const responseTime = new Date().toUTCString()
             console.log(`Client: ${client}, time: ${responseTime} end connection`);
             res.end(`Current time: ${responseTime}`);
